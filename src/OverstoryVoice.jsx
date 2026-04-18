cc3d6fd01122f0e0146b2e03ed49044adc15ac94
heymicky123 <micaela.brookman@gmail.com>
2026-04-17 18:15:01 -0700

    Remove Overstory header, add gif/image container above text stage

--- src/OverstoryVoice.jsx
+++ src/OverstoryVoice.jsx
@@ -124,11 +124,12 @@ export default function OverstoryVoice() {
 
       <div style={{ position: "relative", zIndex: 1, width: "100%", maxWidth: "420px", boxSizing: "border-box" }}>
 
-        <div style={{ marginBottom: "56px" }}>
-          <div style={{ fontSize: "11px", letterSpacing: "0.22em", textTransform: "uppercase", color: "#9B8E7E", marginBottom: "10px" }}>
-            Overstory
-          </div>
-          <div style={{ width: "28px", height: "1px", backgroundColor: "#C4B89A" }} />
+        <div style={{ marginBottom: "40px" }}>
+          <img
+            src={`${import.meta.env.BASE_URL}Memory_test_ferry.gif`}
+            alt=""
+            style={{ width: "100%", display: "block" }}
+          />
         </div>
 
         <div
