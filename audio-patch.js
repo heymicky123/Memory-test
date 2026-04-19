7ecf407dd4cbf20a817b9f72fdeda3c816dc8533
heymicky123 <micaela.brookman@gmail.com>
2026-04-19 10:17:38 -0700

    Fix bar width inconsistency: use absolute pixel positioning instead of flex gap

--- audio-patch.js
+++ audio-patch.js
@@ -1,5 +1,5 @@
 (function () {
-  var BAR_COUNT = 60; // 60 bars @ 2px wide, 3px gap, centered
+  var BAR_COUNT = 60; // 60 bars @ 2px wide, step=5px, total=297px
   var BAR_HEIGHTS = (function () {
     var seed = 42;
     var rand = function () {
@@ -73,11 +73,16 @@
     shell.appendChild(appDiv);
 
     var waveform = document.createElement("div");
-    waveform.style.cssText = "position:absolute;bottom:40px;left:24px;right:24px;height:32px;display:flex;align-items:center;justify-content:center;gap:3px;z-index:5;";
+    waveform.style.cssText = "position:absolute;bottom:40px;left:24px;right:24px;height:32px;display:flex;align-items:center;justify-content:center;z-index:5;";
+
+    var inner = document.createElement("div");
+    inner.style.cssText = "position:relative;width:297px;height:32px;";
+    waveform.appendChild(inner);
+
     for (var i = 0; i < BAR_COUNT; i++) {
       var bar = document.createElement("div");
-      bar.style.cssText = "width:2px;height:" + BAR_HEIGHTS[i] + "px;background-color:#DDD8D0;border-radius:1px;flex-shrink:0;";
-      waveform.appendChild(bar);
+      bar.style.cssText = "position:absolute;left:" + (i * 5) + "px;top:50%;transform:translateY(-50%);width:2px;height:" + BAR_HEIGHTS[i] + "px;background-color:#DDD8D0;border-radius:1px;";
+      inner.appendChild(bar);
       bars.push(bar);
     }
     appDiv.appendChild(waveform);
